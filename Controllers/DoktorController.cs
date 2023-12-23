921fdf6aea01470e694c62cd284c73a08a4af391
DevEmre <osemturhan@gmail.com>
2023-12-23 23:35:38 +0300

    controller_model_update
    controller_model_update

---
+++ Controllers/DoktorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace web_programlama_proje_001.Controllers
+{
+    public class DoktorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/AdminModel.cs
@@ -0,0 +1,6 @@
+﻿namespace web_programlama_proje_001.Models
+{
+    public class AdminModel
+    {
+    }
+}

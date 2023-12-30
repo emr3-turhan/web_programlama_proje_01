101efcfe7ea15732adc99c7fbd8bf24fdafdc575
DevEmre <osemturhan@gmail.com>
2023-12-30 17:47:05 +0300

    Db_update
    Db_update

--- Models/Randevu.cs
+++ Models/Randevu.cs
@@ -2,5 +2,13 @@
 {
     public class Randevu
     {
+        public int Id { get; set; }
+        public DateTime Tarih { get; set; }
+
+        public int DoktorId { get; set; }
+        public int HastaId { get; set; }
+
+        public virtual Doktor Doktor { get; set; }
+        public virtual Hasta Hasta { get; set; }
     }
 }

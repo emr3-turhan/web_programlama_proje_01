b396f0bc97c8b8907b14111e972ba2df07e734a6
DevEmre <osemturhan@gmail.com>
2023-12-30 17:35:13 +0300

    context_update_2
    context_update_2

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,20 @@
-﻿namespace web_programlama_proje_001.Data
+﻿using Microsoft.EntityFrameworkCore;
+using web_programlama_proje_001.Models;
+
+namespace web_programlama_proje_001.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Doktor> Doktorlar { get; set; }
+        public DbSet<Hasta> Hastalar { get; set; }
+        public DbSet<Admin> Adminler { get; set; }
+        public DbSet<AnaBilimDali> AnaBilimDallari { get; set; }
+        public DbSet<Poliklinik> Poliklinikler { get; set; }
+        public DbSet<CalismaSaati> CalismaSaatleri { get; set; }
+        public DbSet<Randevu> Randevular { get; set; }
     }
 }
